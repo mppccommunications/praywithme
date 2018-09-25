c62109cf74001468a6a9a4150e781169224dd40e
mppccommunications <31520535+mppccommunications@users.noreply.github.com>
2018-09-25 12:26:20 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,8 @@
 
 $(function() {
 
-    // Populate using an object literal
-
-    // WTF.init({
-
-    //     heading: [
-    //         "It's a fucking",
-    //         "Check this shit out, a fucking"
-    //     ],
-    //     response: [
-    //         "Already fucking seen one",
-    //         "Give me a-fucking-nother one",
-    //         "Don't really give a fuck"
-    //     ],
-    //     template: [
-    //         "Big @color @animal",
-    //         "Silly @animal with @color fur"
-    //     ],
-    //     animal: [
-    //         "dog",
-    //         "cat",
-    //         "rabbit"
-    //     ],
-    //     color: [
-    //         "red",
-    //         "blue",
-    //         "green",
-    //         "yellow"
-    //     ]
-    // });
-
-    // Populate using a JSON file
-    // WTF.init( 'sample.json' );
-
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc' );
+    WTF.init( '2PACX-1vS7EyBnScpT-oUyUqCDlgvqavKL9Lkb6sct-M8KSdqhKdaFjR2thGp3vn9KIXjRqq3a-4satk0EMLKb' );
 
 });
