c9ead16a4f894c4fcd48b0dfd7c1f2682647077a
mppccommunications <31520535+mppccommunications@users.noreply.github.com>
2018-09-25 12:27:09 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,6 @@ $(function() {
 
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '2PACX-1vS7EyBnScpT-oUyUqCDlgvqavKL9Lkb6sct-M8KSdqhKdaFjR2thGp3vn9KIXjRqq3a-4satk0EMLKb' );
+    WTF.init( 'M8KSdqhKdaFjR2thGp3vn9KIXjRqq3a' );
 
 });
